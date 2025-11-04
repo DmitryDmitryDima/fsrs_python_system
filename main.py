d9a4a5c5622f75d02b57879363c9f80eb707e79c
User <astakamokro@yandex.ru>
2025-11-04 20:23:26 +0300

    deck added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from urllib.request import Request
 
 from fastapi import FastAPI, Depends, Request, HTTPException, status
@@ -5,6 +6,7 @@ from pydantic import BaseModel
 from fsrs import Scheduler, Card, Rating, ReviewLog, State
 from datetime import datetime, timezone
 from sqlmodel import Field, Session, SQLModel, create_engine, select, Column, DateTime
+from uuid import UUID, uuid4
 
 from typing import Annotated, Optional
 
@@ -14,29 +16,51 @@ import uvicorn
 app = FastAPI()
 
 
-# сущность для фронтенд ответа при просмотре карточек
+# dto для фронтенд ответа при просмотре карточек
 class RatedCard(BaseModel):
     card_id: int
     rating: str
+    deck_id: int
 
-# сущность для бэкенд ответа
+# dto для бэкенд ответа
 class BackendAnswerCard(BaseModel):
     card_id: Optional[int] = None
     front_content: Optional[str] = None
     back_content: Optional[str] = None
 
 
-
+# dto для запроса на создание новой карточки
 class NewCard(BaseModel):
     front_content: str
     back_content: str
     with_reversed: bool
 
 
+# dto для запроса на создание новой колоды
+class NewDeck(BaseModel):
+    deck_name:str
+
+class DeckDTO(BaseModel):
+
+    deck_id: int
+    deck_name: str
 
 
 
-# сущность для таблицы
+
+
+
+
+
+
+# сущность колоды для таблицы
+class Deck(SQLModel, table=True):
+    deck_id:int | None = Field(default=None, primary_key=True)
+    user_uuid:UUID
+    deck_name: str = Field(unique=True, nullable=False)
+
+
+# сущность карточки для таблицы
 class DatabaseCard(SQLModel, table=True):
     card_id: int | None = Field(default=None, primary_key=True)
     state: int
@@ -58,9 +82,11 @@ class DatabaseCard(SQLModel, table=True):
     front_content: str
     back_content: str
 
+    deck: int = Field(foreign_key="deck.deck_id")
 
 
-def convertFsrsEntityToDbEntity(fsrsCard:Card, front_content:str, back_content:str):
+
+def convertFsrsEntityToDbEntity(fsrsCard:Card, request_body:NewCard, reversed: bool):
     databaseCard = DatabaseCard()
     #databaseCard.card_id = fsrsCard.card_id
     databaseCard.state = fsrsCard.state.value
@@ -73,9 +99,14 @@ def convertFsrsEntityToDbEntity(fsrsCard:Card, front_content:str, back_content:s
     databaseCard.due = fsrsCard.due
     databaseCard.last_review = fsrsCard.last_review
 
+    if not reversed:
+        databaseCard.front_content = request_body.front_content
+        databaseCard.back_content = request_body.back_content
+    else:
+        databaseCard.front_content = request_body.back_content
+        databaseCard.back_content = request_body.front__content
 
-    databaseCard.front_content = front_content
-    databaseCard.back_content = back_content
+    databaseCard.deck = request_body.deck
     return databaseCard
 
 
@@ -126,16 +157,14 @@ def convertDbEntityToFsrsCardAndMakeReview(dbCard:DatabaseCard, rating:str)->Dat
 
 
 
-#sqlite_file_name = "database.db"
-#sqlite_url = f"sqlite:///{sqlite_file_name}"
+
 
 # postgres - имя пользователя
 # 123 - пароль
 # localhost:5432/postgres - адрес базы данных с таблицами
 postgres_url = "postgresql://postgres:123@localhost:5432/postgres"
 
-#connect_args = {"check_same_thread": False}
-#engine = create_engine(sqlite_url, connect_args=connect_args)
+
 engine = create_engine(postgres_url)
 
 
@@ -161,6 +190,26 @@ create_db_and_tables()
 
 
 
+# список колод для юзера
+# для наглядности можно возвращать количество доступных к изучению карточек
+
+#todo число карточек, доступных к изучению для каждой колоды
+@app.get("/getDecks")
+def getDecks(request:Request, session:SessionDep):
+    role = request.headers.get("role")
+    if (role != "ADMIN" and role != "USER"):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    user_uuid = request.headers.get("uuid")
+
+    statement = select(Deck).filter(Deck.user_uuid==user_uuid)
+
+    deck_list = session.exec(statement).all()
+
+    answer = [DeckDTO(deck_id = x.deck_id, deck_name =  x.deck_name) for x in deck_list]
+
+
+    return answer
 
 
 
@@ -170,15 +219,52 @@ create_db_and_tables()
 
 
 
-# отдельный эндпоинт для получения следующей карточки
-@app.get("/next")
-def next_card(session: SessionDep, request:Request):
+# добавляем колоду
+@app.post("/addDeck")
+def add_deck(request:Request, body:NewDeck, session:SessionDep):
 
-    print(request.headers.get("role"))
+    print(request)
+    role = request.headers.get("role")
+    if (role!="ADMIN" and role!="USER"):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    user_uuid = request.headers.get("uuid")
+
+    add_deck = Deck()
+    add_deck.deck_name = body.deck_name
+    add_deck.user_uuid = user_uuid
+
+    session.add(add_deck)
+    session.commit()
 
 
 
-    statement = select(DatabaseCard).filter(DatabaseCard.due<datetime.now(timezone.utc))
+
+
+# отдельный эндпоинт для получения следующей карточки из колоды
+@app.get("/next/{deck_id}")
+def next_card(session: SessionDep, request:Request, deck_id:int):
+
+    role = request.headers.get("role")
+    if (role != "ADMIN" and role != "USER"):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    user_uuid = request.headers.get("uuid")
+
+    # вытаскиваем колоду, проверяем, что она принадлежит авторизованному пользователю
+    deckSelectStatement = select(Deck).where(Deck.deck_id == deck_id)
+    foundDeck = session.exec(deckSelectStatement).first()
+    if (foundDeck==None):
+        raise HTTPException(status_code=400, detail = "deck doesn't exists")
+
+    if (foundDeck.user_uuid!=user_uuid):
+        raise HTTPException(status = 403, detail="no permission for this id")
+
+
+
+
+
+    statement = select(DatabaseCard).filter(DatabaseCard.due<datetime.now(timezone.utc) and DatabaseCard.deck == deck_id)
     next_card = session.exec(statement).first()
     if (next_card == None):
         answer = BackendAnswerCard()
@@ -200,7 +286,21 @@ def next_card(session: SessionDep, request:Request):
 
 #@app.post("/api/tools/cards/repetition")
 @app.post("/repetition")
-def view_cards(requestBody:RatedCard, session: SessionDep):
+def view_cards(requestBody:RatedCard, session: SessionDep, request:Request):
+    role = request.headers.get("role")
+    if (role != "ADMIN" and role != "USER"):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    user_uuid = request.headers.get("uuid")
+
+    # вытаскиваем колоду, проверяем, что она принадлежит авторизованному пользователю
+    deckSelectStatement = select(Deck).where(Deck.deck_id == requestBody.deck_id)
+    foundDeck = session.exec(deckSelectStatement).first()
+    if (foundDeck == None):
+        raise HTTPException(status_code=400, detail="deck doesn't exists")
+
+    if (foundDeck.user_uuid != user_uuid):
+        raise HTTPException(status=403, detail="no permission for this id")
 
 
 
@@ -208,7 +308,7 @@ def view_cards(requestBody:RatedCard, session: SessionDep):
     #получаем карточку по id
     dbCard = session.get(DatabaseCard, requestBody.card_id)
 
-    print(dbCard)
+
 
 
 
@@ -218,22 +318,8 @@ def view_cards(requestBody:RatedCard, session: SessionDep):
     session.add(dbCard)
     session.commit()
 
-    # далее посылаем следующего кандидата
 
-    statement = select(DatabaseCard).filter(DatabaseCard.due < datetime.now(timezone.utc))
-    next_card = session.exec(statement).first()
-    if (next_card == None):
-        answer = BackendAnswerCard()
-        answer.back_content = None
-        answer.front_content = None
-        answer.card_id = None
-        return answer
-    else:
-        answer = BackendAnswerCard()
-        answer.back_content = next_card.back_content
-        answer.front_content = next_card.front_content
-        answer.card_id = next_card.card_id
-        return answer
+
 
 
 
@@ -248,8 +334,29 @@ def view_cards(requestBody:RatedCard, session: SessionDep):
 
 
 #@app.post("/api/tools/cards/add")
-@app.post("/add")
-def add_card(requestBody:NewCard, session: SessionDep ):
+@app.post("/addCard")
+def add_card(requestBody:NewCard, session: SessionDep, request:Request):
+    role = request.headers.get("role")
+    user_uuid = request.headers.get("uuid")
+    if (role != "ADMIN" and role != "USER"):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    # вытаскиваем колоду, проверяем, что она принадлежит авторизованному пользователю
+    deckSelectStatement = select(Deck).where(Deck.deck_id == requestBody.deck_id)
+    foundDeck = session.exec(deckSelectStatement).first()
+    if (foundDeck == None):
+        raise HTTPException(status_code=400, detail="deck doesn't exists")
+
+    if (foundDeck.user_uuid != user_uuid):
+        raise HTTPException(status=403, detail="no permission for this id")
+
+
+
+
+
+
+
+
 
     scheduler = Scheduler()
     # создаем дефолтную карту
@@ -260,14 +367,14 @@ def add_card(requestBody:NewCard, session: SessionDep ):
 
     card, review_log = scheduler.review_card(card, rating)
 
-    dbCard = convertFsrsEntityToDbEntity(card, requestBody.front_content, requestBody.back_content)
+    dbCard = convertFsrsEntityToDbEntity(card, requestBody)
 
     session.add(dbCard)
 
     session.commit()
 
     if (requestBody.with_reversed):
-        dbCardReversed = convertFsrsEntityToDbEntity(card, requestBody.back_content, requestBody.front_content)
+        dbCardReversed = convertFsrsEntityToDbEntity(card, requestBody, reversed = True)
 
         session.add(dbCardReversed)
 
